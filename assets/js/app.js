52de898b76699a2e0e4cb5e06ff1bc97a052da2c
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-05 17:09:12 -0700

    Adding on click function to animate and still gifs within app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,6 +33,7 @@ function displayTopicInfo() {
         for (var i = 0; i < 10; i++) {
             var topicDiv = $("<div>");
             var topicImage = $("<img>");
+            topicImage.addClass("gif");
             topicImage.attr('src', results[i].images.fixed_height_still.url);
             topicImage.attr('data-still', results[i].images.fixed_height_still.url);
             topicImage.attr('data-animate', results[i].images.fixed_height.url);
@@ -43,6 +44,21 @@ function displayTopicInfo() {
         }
     })
 }
+
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+
+    if (state === "still") {
+        $(this).attr('data-state', "animate");
+        $(this).attr('src', $(this).attr('data-animate'));
+    }
+    else {
+        $(this).attr('data-state', "still");
+        $(this).attr('src', $(this).attr('data-still'));
+    }
+})
+
 
 $(document).on("click", ".topic", displayTopicInfo);
 
